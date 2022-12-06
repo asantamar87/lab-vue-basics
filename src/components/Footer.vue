f1371267edfb8550f2c2bb4386d13b00a359986d
aldosantamaria <asantama@jaumebalmes.net>
2022-12-06 14:49:49 +0100

    Lab done

---
+++ src/components/Footer.vue
@@ -0,0 +1,22 @@
+<!-- TEMPLATE -->
+<template>
+    <footer class="footer">
+      <div class="content has-text-centered">
+        <p>
+          <strong>Bulma</strong> by <a href="https://jgthms.com">Jeremy Thomas</a>. The source code is licensed
+          <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
+          is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
+        </p>
+      </div>
+    </footer>
+    
+    </template> 
+    
+    <!-- SCRIPT COMPOSITION API (JavaScript)-->
+    <script setup>
+    // import{ref} from 'vue'
+    </script>
+    
+    <!-- STYLE -->
+    <style>
+    </style>
